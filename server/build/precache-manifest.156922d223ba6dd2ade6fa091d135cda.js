0ef3dd69b800f6f95a4d9ea268ae23af9afb173c
Mostafa Tavassoli Norouzi <68356134+mosTavassoli@users.noreply.github.com>
2021-01-28 17:33:28 +0100

    feat: send file build

---
+++ server/build/precache-manifest.156922d223ba6dd2ade6fa091d135cda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae142be89542b4fe02ea2b1bb6a772f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35e5047bc5e621d7eec",
+    "url": "/static/css/2.761498e4.chunk.css"
+  },
+  {
+    "revision": "860ae96bb49f748c5fb9",
+    "url": "/static/css/main.d1b3c306.chunk.css"
+  },
+  {
+    "revision": "c35e5047bc5e621d7eec",
+    "url": "/static/js/2.907c76d6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.907c76d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860ae96bb49f748c5fb9",
+    "url": "/static/js/main.04fe4996.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
